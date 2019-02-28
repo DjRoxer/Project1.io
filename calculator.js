63a0d78fe10d21d6bdd957d04d4ff5f03956b14a
Dillon <dillonroxer@gmail.com>
2019-02-28 13:19:13 -0700

    NO_MORE_BUGS!

--- calculator.js
+++ calculator.js
@@ -40,34 +40,39 @@ function inputEqual()
 	//The fallowing for loop goes through the string and save each number and op in a new array in the order they appear. 
 	for (var i=0; i <str.length;i++)
 	{
-		if(i==str.length-1)
-		{
-			console.log(str[i]+ "\n");
-			elements[ind] = parseInt(str[i]);
-			break;
-		}
+		// if(i==str.length-1)
+		// {
+		// 	console.log("Last single element");
+		// 	console.log(str[i]+ "\n");
+		// 	elements[ind] = parseInt(str[i]);
+		// 	break;
+		// }
 		for (var j=i; j<str.length;j++)
 		{
 			if(str[j]=='*' || str[j]=='/' || str[j]=='-' || str[j]=='+')
 			{
 				elements[ind] = parseInt(str.substring(i,j));
-				console.log(elements[ind]+ "\n")
+				
 
 				elements[ind +1] = str[j];
-				console.log(elements[ind+1]+ "\n")
+			
 				ind = ind +2;
-				i=j;	
+				i=j;
+
 				break;
 
 			}
-			// if (j==str.length-1)
-			// {
-			// 	elements[ind]= parseInt(str.substring(i,str.length));
-			// 	i = str.length;
-			// 	break;
-			// }
+			if (j==str.length-1)
+			{
+				elements[ind]= parseInt(str.substring(i,str.length));
+			
+				i = str.length;
+				break;
+			}
 		}
+
 	}
+	console.log(elements);
 
 
 		var ans = calc(elements);
@@ -76,85 +81,91 @@ function inputEqual()
 
 	//$("#number_input").val(elements[1]);
 }
-function calc(element)
+
+//arr.slice(x,y) will RETURN an array starting at x to y-1
+function calc(elements)
 {
-	var op = element[1];
-	if (element.length == 3)
+
+
+	if (elements.length == 3)
+		{
+			return base(elements);
+		}
+
+
+	var length = elements.length;
+	var ind = 1;
+	var newArr = elements;
+	var high_orders = 1;
+
+
+	while (high_orders)
 	{
+		if (newArr.length == 3)
+		{
+			return base(newArr);
+		}
+
+		if (ind >= newArr.length)
+		{
+			high_orders = 0;
+			ind =1 ; 
+			break;
+		}
 		
-		if (op == '+')
+
+		if((newArr[ind] == '*' || newArr[ind]== '/') && ((newArr.length-ind) > 3) )
 		{
-			return add(element[0], element[2]);
+			console.log("operating: " + newArr[ind]);
+			var subArr = newArr.slice(ind-1,ind+2);
+			var subAns = base (subArr);
+			var head = newArr.slice(0,ind-1);
+			var tail = newArr.slice(ind+1, newArr.length);
+			tail[0] = subAns;
+			newArr = head.concat(tail);
+			ind = ind -2;
 		}
-		if (op == '-')
-		{
-			return sub(element[0], element[2]);
-		}
-		if (op == '/')
-		{
-			return div(element[0], element[2]);
-		}
-		if (op == '*')
-		{
-			return mult(element[0], element[2]);
-		}
+
+
+		ind = ind +2;
 	}
-	console.log("length is: " + element.length ); 
-	if(element.length >=5)
+
+	ind =1;
+	while (ind<newArr.length)
 	{
-		var op2 = element[3];
-		if (op == '+')
+		if (newArr.length == 3)
 		{
-			if(op2 == '*' || op2 == '/')
-			{
-				var mid = calc(element.splice(2,5));
-				var end = [];
-				
-				end = element.splice(5,element.length)
-				console.log("end: " + end );
-				
-				console.log(mid + " + " + end);
-				end.unshift(mid);
-				
-				return (element[0] + calc(end));
-			}
-			else
-			{
-				var head = element[0]+element[2];
-				var end = element.splice(3,element.length);
-				console.log("end: " + end);
-				console.log("elements: " + end.unshift(head));
-				return calc(end.unshift(head));
-			}
+			return base(newArr);
 		}
-		if (op == '-')
-		{
-			return sub(element[0], element[2]);
-		}
-		if (op == '/')
-		{
-			return div(element[0], element[2]);
-		}
-		if (op == '*')
-		{
-			return mult(element[0], element[2]);
-		}
+		console.log("operating: " + newArr[ind]);
+		var subArr = newArr.slice(ind-1,ind+2);
+		var subAns = base (subArr);
+		var head = newArr.slice(0,ind-1);
+		var tail = newArr.slice(ind+1, newArr.length);
+		tail[0] = subAns;
+		newArr = head.concat(tail);
+			
 	}
-	return (element);
+	return newArr;
 }
-function add(x, y)
+function base(elements)
 {
-	return x + y;
-}
-function sub(x, y)
-{
-	return x-y;
-}
-function mult(x,y)
-{
-	return x * y;
-}
-function div(x,y)
-{
-	return x/y;
+	var op = elements[1];
+	if (op == '+')
+	{
+		return elements[0] + elements[2];
+	}
+	if (op == '-')
+	{
+		return elements[0] - elements[2];
+	}
+	if (op == '/')
+	{
+		return elements[0] / elements[2];
+	}
+	if (op == '*')
+	{
+		return elements[0] * elements[2];
+	}
+	return elements;
 }
